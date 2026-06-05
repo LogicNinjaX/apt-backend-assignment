490324be8ad35199dc15cd9d579db44fc807e866
Nitish <mail.logicninjax@gmail.com>
2026-06-05 12:00:10 +0530

    first commit

---
+++ src/main/java/com/nitish/apt_assignment/AptAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.nitish.apt_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitish/apt_assignment/AptAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitish.apt_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
